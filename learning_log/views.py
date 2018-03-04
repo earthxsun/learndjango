af10674a6f7421424aabfe4c90f40b935b867172
earthx <1057229118@qq.com>
2018-03-04 19:30:09 +0800

    2018-03-04 提交

--- learning_log/views.py
+++ learning_log/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
+from .forms import TopicForm, EntryForm
+from .models import Topic
 # Create your views here.
+
+
+def index(request):
+
+    return render(request, 'learning_log/index.html')
+
+
+def topics(request):
+
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_log/topics.html', context)
+
+
+def topic(request, topic_id):
+
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries':entries}
+    return render(request, 'learning_log/topic.html', context)
+
+
+def new_topic(request):
+
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_log:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_log/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_log:topic',
+                                                args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_log/new_entry.html', context)
