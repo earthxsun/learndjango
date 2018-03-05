16eb80ce3a39583b0343452ee2130d79b8fae42a
earthx <1057229118@qq.com>
2018-03-05 23:50:05 +0800

    第19章完成

--- users/urls.py
+++ users/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     url(r'^login/$', login, {'template_name': 'users/login.html'},
         name='login'),
     url(r'^logout/$', views.logout_view, name='logout'),
+    url(r'^register/$', views.register, name='register'),
 ]
