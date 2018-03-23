32e2efb8dc20376eb14dfd48618d4952aa966fa5
earthxsun <earthx@126.com>
2018-03-23 18:18:09 +0800

    20180323 第六章3节

--- books/views.py
+++ books/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.shortcuts import HttpResponse
 # Create your views here.
+
+
+def displaymeta(request):
+    values = request.META.items()
+    # values.sort()
+    html = []
+    for k, v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' %(k, v))
+    return HttpResponse('<table>%s</table>' % '\n'.join(html))
+
+
+def search_form(request):
+    return render(request, 'search_form.html')
+
+
+def search(request):
+    if 'q' in request.GET:
+        message = 'You searched for: %r' % request.GET['q']
+    else:
+        message = 'You submitted an empty form.'
+    return HttpResponse(message)
